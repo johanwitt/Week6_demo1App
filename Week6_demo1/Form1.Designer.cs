69fe9e67a0ff77bf6fbcf8d1659f14bb7ce74ef3
ongchangcoj <ongchangcoj@B101PROJ>
2024-04-10 09:16:26 +1200

    Add project files.

---
+++ Week6_demo1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Week6_demo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ID_textBox = new System.Windows.Forms.TextBox();
+            this.Name_textBox = new System.Windows.Forms.TextBox();
+            this.DOB_textBox = new System.Windows.Forms.TextBox();
+            this.Address_textBox = new System.Windows.Forms.TextBox();
+            this.Phone_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(336, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ID_textBox
+            // 
+            this.ID_textBox.Location = new System.Drawing.Point(32, 67);
+            this.ID_textBox.Name = "ID_textBox";
+            this.ID_textBox.Size = new System.Drawing.Size(163, 20);
+            this.ID_textBox.TabIndex = 1;
+            // 
+            // Name_textBox
+            // 
+            this.Name_textBox.Location = new System.Drawing.Point(32, 153);
+            this.Name_textBox.Name = "Name_textBox";
+            this.Name_textBox.Size = new System.Drawing.Size(163, 20);
+            this.Name_textBox.TabIndex = 2;
+            // 
+            // DOB_textBox
+            // 
+            this.DOB_textBox.Location = new System.Drawing.Point(32, 250);
+            this.DOB_textBox.Name = "DOB_textBox";
+            this.DOB_textBox.Size = new System.Drawing.Size(163, 20);
+            this.DOB_textBox.TabIndex = 3;
+            // 
+            // Address_textBox
+            // 
+            this.Address_textBox.Location = new System.Drawing.Point(491, 67);
+            this.Address_textBox.Name = "Address_textBox";
+            this.Address_textBox.Size = new System.Drawing.Size(163, 20);
+            this.Address_textBox.TabIndex = 4;
+            // 
+            // Phone_textBox
+            // 
+            this.Phone_textBox.Location = new System.Drawing.Point(491, 153);
+            this.Phone_textBox.Name = "Phone_textBox";
+            this.Phone_textBox.Size = new System.Drawing.Size(163, 20);
+            this.Phone_textBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "lID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "DOB";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(488, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(488, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Phone";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Phone_textBox);
+            this.Controls.Add(this.Address_textBox);
+            this.Controls.Add(this.DOB_textBox);
+            this.Controls.Add(this.Name_textBox);
+            this.Controls.Add(this.ID_textBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ID_textBox;
+        private System.Windows.Forms.TextBox Name_textBox;
+        private System.Windows.Forms.TextBox DOB_textBox;
+        private System.Windows.Forms.TextBox Address_textBox;
+        private System.Windows.Forms.TextBox Phone_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
