d30452a32f459ba5f73f41c292a96d25ef629bb3
johanwitt <johan.witt23@gmail.com>
2024-05-20 09:54:57 +1200

    Insert into command

--- Week6_demo1/Form1.cs
+++ Week6_demo1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,8 +18,24 @@ namespace Week6_demo1
             InitializeComponent();
         }
 
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;
+            Initial Catalog=Student;Integrated Security=True;");
+            
+
         private void button1_Click(object sender, EventArgs e)
         {
+            conn.Open();
+
+            // Insert into Stud_details values (ID_textBox.Text, Name_textBox.Text..)
+            string SqlCmd = "Insert into Stud_details values('" + ID_textBox.Text + "','" + Name_textBox.Text + "','" + DOB_textBox.Text + "','" + Address_textBox.Text + "','" + Phone_textBox.Text + "')";
+            
+            SqlCommand cmd = new SqlCommand(SqlCmd, conn);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            MessageBox.Show("Insert done!");
+            
 
         }
     }
